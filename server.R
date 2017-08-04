0ded06bbfc199940cfbe6f1d3c3215538e2983a8
Sam Abbott <sam.abbott@bristol.ac.uk>
2017-08-04 16:58:27 +0100

    added rough outline of models

--- server.R
+++ server.R
@@ -3,12 +3,81 @@ library(shiny)
 library(shinydashboard)
 library(shinyBS)
 library(tidyverse)
-
+library(plotly)
+library(idmodelr)
+library(DT)
 ## Stop spurious warnings
 options(warn = -1)
 
 shinyServer(function(input, output) {
   
+  ## Reactive slider for number of infected
+  output$init_infected <- renderUI({
+    sliderInput(inputId = "I_0", 
+                label = "Initially Infected:",
+                min = 0,
+                max = input$N,
+                value = 1)
+  })
+  
+  model_sim <- reactive({
+  ##across all models
+  times <- seq(0, input$maxtime, 0.1)
+  
+    if (input$model %in% "SI_ode") {
+      ## Model
+      model <- SI_ode
+      sim_fn <- solve_ode
+      ##parameters
+      params <- data.frame(beta = input$beta)
+      
+      ##initial pop
+      inits <- data.frame(S = input$N - input$I_0, I = input$I_0)
+    }else if (input$model %in% "SI_demo_ode") {
+      ## Model
+      model <- SI_demo_ode
+      sim_fn <- solve_ode
+      ##parameters
+      params <- data.frame(beta = input$beta, mu = 1/input$mu)
+      
+      ##initial pop
+      inits <- data.frame(S = input$N - input$I_0, I = input$I_0)
+    }else if (input$model %in% "SEI_ode") {
+      ## Model
+      model <- SEI_ode
+      sim_fn <- solve_ode
+      ##parameters
+      params <- data.frame(beta = input$beta, gamma = 1/input$gamma)
+      
+      ##initial pop
+      inits <- data.frame(S = input$N - input$I_0, E = 0, I = input$I_0)
+    }else if (input$model %in% "SEI_demo_ode") {
+      ## Model
+      model <- SEI_demo_ode
+      sim_fn <- solve_ode
+      ##parameters
+      params <- data.frame(beta = input$beta, gamma = 1/input$gamma, mu = 1/input$mu)
+      
+      ##initial pop
+      inits <- data.frame(S = input$N - input$I_0, E = 0, I = input$I_0)
+    }
+    
+    model_sim <- simulate_model(model = model, 
+                                sim_fn = sim_fn, 
+                                inits = inits, 
+                                params = params,
+                                times = times,
+                                as_tibble = TRUE)
+  })
+  
+  output$plot_model_traj <- renderPlot({
+    model_sim() %>% 
+      plot_model
+  })
+  
+  output$model_sim_results <- DT::renderDataTable({
+    model_sim()
+  })
   output$downloadData2 <- downloadHandler(filename = "ui.R",
                                           content = function(file) {
                                             file.copy("ui.R", file, overwrite = TRUE)
--- ui.R
+++ ui.R
@@ -4,11 +4,14 @@ library(shinydashboard)
 library(shinyBS)
 library(tidyverse)
 library(rmarkdown)
+library(plotly)
+library(idmodelr)
+library(DT)
 
 sidebar <- dashboardSidebar(
   hr(),
-  sidebarMenu(id = "tabs",
-              menuItem("Explore Model Dynamics", tabName = "model", icon = icon("line-chart")),
+  sidebarMenu(id = "menu",
+              menuItem("Explore Model Dynamics", tabName = "exp-model", icon = icon("line-chart")),
               menuItem("About", tabName = "readme", icon = icon("mortar-board"), selected = TRUE),
               menuItem("Code",  icon = icon("code"),
                        menuSubItem("Github", href = "https://github.com/seabbs/exploreidmodels", icon = icon("github")),
@@ -16,6 +19,47 @@ sidebar <- dashboardSidebar(
                        menuSubItem("server.R", tabName = "server", icon = icon("angle-right"))
               )
   ),
+  conditionalPanel(condition = 'input.menu == "exp-model"',
+             selectInput("model",
+                         "Select a model:",
+                         list(SI = "SI_ode",
+                              `SI with demographics` = "SI_demo_ode",
+                              SEI = "SEI_ode",
+                              `SEI with demographics` = "SEI_demo_ode")),
+             sliderInput("N",
+                         "Population:",
+                         value = 1000,
+                         min = 10, 
+                         max = 10000,
+                         step = 10),
+             uiOutput("init_infected"),
+             sliderInput("beta", 
+                         "Beta:",
+                         value = 2,
+                         min = 0, 
+                         max = 50,
+                         step = 0.1),
+             conditionalPanel(condition = "input.model == 'SEI_ode' || input.model == 'SEI_demo_ode'",
+                              sliderInput("gamma",
+                                          "Exposure period:",
+                                          value = 1,
+                                          min = 0,
+                                          max = 100)
+             ),
+             conditionalPanel(condition = "input.model == 'SEI_demo_ode' ||
+                              input.model ==  'SI_demo_ode'",
+                              sliderInput("mu",
+                                          "Life Expectancy:",
+                                          value = 80,
+                                          min = 0, 
+                                          max = 1000,
+                                          step = 1)),
+             radioButtons("maxtime",
+                         "Timespan for model:",
+                         selected = 10,
+                         choices = c(10, 50, 100, 1000), 
+                         inline = TRUE)
+                   ),
   hr(),
   helpText("Developed by ", a("Sam Abbott", href = "http://samabbott.co.uk"), 
            style = "padding-left:1em; padding-right:1em;position:absolute; bottom:1em; ")
@@ -23,9 +67,23 @@ sidebar <- dashboardSidebar(
 
 body <- dashboardBody(
   tabItems(
-    tabItem(tabName = "model",
-             conditionalPanel(condition = 'input.menu == "model"')
-    ),
+    tabItem(tabName = "exp-model",
+            fluidRow(
+              tabBox(width = 12, 
+                     title = "Model Plots", 
+                     side = "right",
+                     tabPanel(title = "Trajectories",
+                              plotOutput("plot_model_traj")
+                              )
+                     ),
+                     tabBox(width = 12, 
+                            title = "Model Statistics", 
+                            side = "right",
+                            tabPanel(title = "Trajectories",
+                                     DT::dataTableOutput("model_sim_results")
+                            )
+                            )
+    )),
     tabItem(tabName = "readme",
             withMathJax(), 
             includeMarkdown("README.md")
