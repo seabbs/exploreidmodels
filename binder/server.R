7b2c321ba2fad3ef4ea6fb20ec684a0c8fe3a463
seabbs <s.e.abbott21@gmail.com>
2019-05-01 10:18:14 +0000

    moved to dockerfile based app et up for binder

--- binder/server.R
+++
@@ -1,9 +0,0 @@
-## Dummy server for shiny binder deployment
-## Set the working directory
-setwd("..")
-
-## Load Server 
-source("server.R")
-
-
-
--- binder/ui.R
+++
@@ -1,8 +0,0 @@
-## Dummy UI for binder shiny deployment
-## Set the working directory
-setwd("..")
-
-## Load UI
-source("ui.R")
-
-
