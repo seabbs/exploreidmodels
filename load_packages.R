e61616721b74a16d7c98a4350d9d6dfe5ffb4cf6
Sam Abbott <contact@samabbott.co.uk>
2022-02-08 11:41:11 +0000

    Update load_packages.R

--- load_packages.R
+++ load_packages.R
@@ -1,6 +1,6 @@
 ## Load packages
 packages <- list("shiny", "shinydashboard", "shinyBS", "magrittr", "dplyr", 
-                   "rmarkdown", "plotly", "DT", "idmodelr", "shinyWidgets",
+                  "markdown", "rmarkdown", "plotly", "DT", "idmodelr", "shinyWidgets",
                  "shinycssloaders")
 
 ## Assign usernames to github packag
